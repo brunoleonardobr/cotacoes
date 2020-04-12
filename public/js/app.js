6e1490384f7cb4f95c7c3a6ba013d5f31d9d7e05
Bruno Leonardo <brunoleonardodev@gmail.com>
2020-04-12 17:22:10 -0300

    primeiro commit

---
+++ public/js/app.js
@@ -0,0 +1,37 @@
+console.log('javascript no frontend');
+
+const cotacoesForm = document.querySelector('form')
+const mainmensage = document.querySelector('h3')
+const price = document.querySelector('#price')
+const price_open = document.querySelector('#price_open')
+const day_high = document.querySelector('#day_high')
+const day_low = document.querySelector('day_low')
+
+cotacoesForm.addEventListener('submit',(event)=>{
+    mainmensage.innerText = 'buscando...'
+    event.preventDefault()
+    const ativo = document.querySelector('input').value
+
+    if(!ativo){
+        mainmensage.innerText = "O ativo deve ser informado"
+        return;
+    }
+
+    fetch(`http://localhost:3000/cotacoes?ativo=${ativo}`).then((response)=>{
+        response.json().then((data)=>{
+            if(data.error){
+                mainmensage.innerText = `Alguma coisa deu errado - ${data.error.mensage}, código ${data.error.code}`;
+                price.innerHTML = `${data.error.mensage} | código ${data.error.code}`
+                price_open.innerHTML = ''
+                day_high.innerHTML = ''
+                day_low.innerHTML = ''
+            }else{
+                mainmensage.innerText = data.symbol
+                price.innerHTML = `price: ${data.price}`
+                price_open.innerHTML = `price open: ${data.price_open}`
+                day_high.innerHTML = `day high: ${data.day_high}`
+                day_low.innerHTML = `day low: ${data.day_low}`
+            }
+        })
+    })
+})
---
+++ src/util/cotacao.js
@@ -0,0 +1,33 @@
+const request = require('request')
+
+const api_token = 'PVmJlbfPBQlolCX4kHuJBP0oHAYezYiDMATQvmvRnZk3LVsPcfAHUCO1euKg'
+
+const cotacao = (symbol,callback) => {
+    
+    const url = `https://www.worldtradingdata.com/api/v1/stock?symbol=${symbol}&api_token=${api_token}`
+
+    request({url:url,json:true},(err,response)=>{
+        if(err){
+            callback({
+                mensage: `Something went wrong: ${err}`,
+                code : 500
+            }, undefined)
+            return;
+        }
+
+        if(response.body === undefined || response.body.data=== undefined){
+            callback({
+                mensage : `No data found`,
+                code : 404
+            }, undefined)
+            return;
+        }
+
+        const parsedJSON = response.body.data[0]
+        const {symbol, price_open, price, day_high, day_low} = parsedJSON
+
+        callback(undefined,{symbol, price_open, price, day_high, day_low})
+    })
+}
+
+module.exports = cotacao
